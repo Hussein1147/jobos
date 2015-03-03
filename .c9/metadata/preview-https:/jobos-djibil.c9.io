544090f06e29b0506e80d663baf79d1f4580563f
Rohit Karki <rohit@live.com>
2015-03-03 07:05:58 +0000

    payment

---
+++ .c9/metadata/preview-https:/jobos-djibil.c9.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://jobos-djibil.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://jobos-djibil.c9.io","previewer":"preview.browser"},"timestamp":1425359430723,"hash":"67452301efcdab8998badcfe10325476c3d2e1f0"}
--- Payment-test.py
+++ Payment-test.py
@@ -7,6 +7,22 @@ import stripe
 
 cgitb.enable()
  
-print 'Content-Type: text/json'
+print 'Content-Type: Application/json'
 
-print 'hello'
+stripe.api_key = 'pk_test_mjZsJALdxDNEccGVqWPBzX73'
+ 
+#4
+json_data = sys.stdin.read()
+json_dict = json.loads(json_data)
+ 
+
+stripeAmount = json_dict['stripeAmount']
+stripeCurrency = json_dict['stripeCurrency']
+stripeToken = json_dict['stripeToken']
+stripeDescription = json_dict['stripeDescription']
+ 
+#6
+json_response = stripe.Charge.create(amount=stripeAmount, currency=stripeCurrency, card=stripeToken, description=stripeDescription)
+ 
+print json_response
+
